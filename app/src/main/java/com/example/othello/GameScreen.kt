056ecded8cef8dcc00aba22e2df1b35e0494c58c
jonathankilit <Jonathan.kilit@hotmail.com>
2023-11-09 20:14:01 +0100

    Grid with tiles added in GameScreen

--- app/src/main/java/com/example/othello/GameScreen.kt
+++ app/src/main/java/com/example/othello/GameScreen.kt
@@ -1,26 +1,55 @@
 package com.example.othello
 
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun GameScreen() {
-    Row(
+    Column(
         modifier = Modifier
-            .fillMaxWidth()
+            .fillMaxSize()
             .padding(16.dp),
-        horizontalArrangement = Arrangement.Center,
-        verticalAlignment = Alignment.CenterVertically
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Button(onClick = { /* TODO: Implement start game */ }) {
-            Text("GameScreen")
+        Spacer(modifier = Modifier.height(16.dp))
+
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(8),
+            modifier = Modifier.fillMaxSize()
+        ) {
+            items(64) { index ->
+                tiles(color = Color.Black)
+                //call what is suppsoed to be inside like cardview
+            }
         }
+    }
+}
+
+@Composable
+fun tiles (color: Color){
+    Button(
+        onClick = { /*TODO*/ },
+        modifier = Modifier.aspectRatio(1f),
+        colors= ButtonDefaults.buttonColors(containerColor = color),
+        shape = CircleShape,
+    ) {
+        
     }
 }
