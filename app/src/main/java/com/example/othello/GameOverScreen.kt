6a028bc47a7454842c3d58c9035194688d0ecbcd
edvinmujanovic <edvin.mujanovic@gmail.com>
2023-11-09 19:40:57 +0100

    Base for GameOverScreen. Began implementing it a bit

---
+++ app/src/main/java/com/example/othello/GameOverScreen.kt
@@ -0,0 +1,46 @@
+package com.example.othello
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+
+
+//FIXA DENNA, Implementera en kallelse från Gamescreen också där man kollar if gameIsOver och skicka in dem parametrar som behövs hit
+@Composable
+fun GameOverScreen(/*winningPlayer: Player, stats String,*/ onRestart: () -> Unit) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("Game Over!", style = MaterialTheme.typography.titleLarge)
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+      //  Text("Winner: ${winningPlayer.name}", style = MaterialTheme.typography.body1)
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+      //  Text("Stats: $stats", style = MaterialTheme.typography.body2)
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(onClick = onRestart) {
+            Text("Restart Game")
+            //FIX
+        }
+    }
+}
