2caef1677030178f0368f9a86b3f19a0baa130b2
edvinmujanovic <edvin.mujanovic@gmail.com>
2023-11-10 16:33:46 +0100

    Fixed a settings screen where you can change the darkmode, fixed a composable to it in mainactivity navhost and added it to the Screen class etc.

--- app/src/main/java/com/example/othello/Screen.kt
+++ app/src/main/java/com/example/othello/Screen.kt
@@ -3,7 +3,8 @@ package com.example.othello
 sealed class Screen(val route: String) {
     object Start : Screen(route = "start")
     object Game : Screen(route = "game")
-
     object GameOver : Screen(route = "gameover")
+
+    object Settings : Screen(route = "settings")
 
 }
---
+++ app/src/main/java/com/example/othello/SettingsScreen.kt
@@ -0,0 +1,75 @@
+package com.example.othello
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.example.othello.R
+import com.example.othello.Screen
+
+@Composable
+fun SettingsScreen(navController: NavController) {
+    var isDarkMode by remember { mutableStateOf(false) }
+
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.background
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxSize()
+        ) {
+            Text(
+                text = "Settings",
+                style = MaterialTheme.typography.titleLarge,
+                modifier = Modifier.padding(bottom = 16.dp)
+            )
+
+            // Dark Mode toggle
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 8.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = "Dark Mode",
+                    style = MaterialTheme.typography.bodyMedium,
+                    modifier = Modifier.weight(1f)
+                )
+
+                Switch(
+                    checked = isDarkMode,
+                    onCheckedChange = { isChecked ->
+                        isDarkMode = isChecked
+                        // Apply your dark mode logic here
+                    }
+                )
+            }
+
+            // more settings......
+
+            Spacer(modifier = Modifier.weight(1f))
+
+            // Back button (TA BORT OM DET REDAN FINNS I VANLIGA MODELLEN!!!
+            Button(
+                onClick = {
+                    navController.navigateUp()
+                }
+            ) {
+                Text("Back")
+            }
+        }
+    }
+}
