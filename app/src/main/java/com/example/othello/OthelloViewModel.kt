cc1fe8c5053e808e3dd2f29ab31f23567117cd36
jonathankilit <Jonathan.kilit@hotmail.com>
2023-11-10 15:44:44 +0100

    Composables added to ViewModel

--- app/src/main/java/com/example/othello/OthelloViewModel.kt
+++ app/src/main/java/com/example/othello/OthelloViewModel.kt
@@ -2,23 +2,35 @@ package com.example.othello
 
 import androidx.compose.runtime.Composable
 
-
 data class Tile(
     val x: Int,
     val y: Int,
-    val isBlack: Boolean,
-    val isWhite: Boolean
+    var isBlack: Boolean,
+    var isWhite: Boolean
 )
 
-@Composable
-fun flip(tile: Tile){
-    if (tile.isBlack){
-        tile.isBlack=false
-        tile.isWhite=true
-        return tile;
+fun flip(tile: Tile) {
+
+    if (tile.isBlack) {
+        tile.isBlack = false
+        tile.isWhite = true
+    } else {
+        tile.isBlack = true
+        tile.isWhite = false
     }
 }
+@Composable
+fun putBlack(tile: Tile){
+    tile.isBlack=true
+    tile.isWhite=false
 
+}
+@Composable
+fun putWhite(tile:Tile){
+    tile.isWhite=true
+    tile.isBlack=false
+
+}
 
 class OthelloViewModel {
 
