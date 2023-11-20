d1c48b64cc3c0171bd26dd6b002da4f15666f9e9
edvinmujanovic <edvin.mujanovic@gmail.com>
2023-11-20 19:18:03 +0100

    Started working on LobbyScreen and LobbyViewModel. Gave it a base we can work from

--- app/src/main/java/com/example/othello/LobbyScreen.kt
+++ app/src/main/java/com/example/othello/LobbyScreen.kt
@@ -1,2 +1,73 @@
 package com.example.othello
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import io.garrit.android.multiplayer.Game
+import io.garrit.android.multiplayer.Player
+import io.garrit.android.multiplayer.ServerState
+import io.garrit.android.multiplayer.SupabaseService
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+@OptIn(ExperimentalCoroutinesApi::class)
+@Composable
+fun LobbyScreen(viewModel: LobbyViewModel = viewModel()) {
+    val serverState = SupabaseService.serverState.collectAsState()
+    //val users = SupabaseService.users.collectAsState() //PÅ DETTA SÄTT?
+
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.background
+    ) {
+        when (serverState.value) {
+            ServerState.NOT_CONNECTED -> {
+                // Fix not connected state
+            }
+            ServerState.LOADING_LOBBY -> {
+                // Fix loading lobby state
+            }
+            ServerState.LOBBY -> {
+                LobbyContent(/*users.value, games.value,*/ viewModel)
+            }
+            ServerState.LOADING_GAME -> {
+                // Fix loading game state
+            }
+            ServerState.GAME -> {
+                // Fix game state
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalCoroutinesApi::class)
+@Composable
+fun LobbyContent(
+   // users: List<Player>, vänta med denna
+    viewModel: LobbyViewModel
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        // Display list of users in the lobby
+       /* Text("Users in the Lobby:") Vänta med denna
+        users.forEach { user ->
+            Text(user.name)
+        } */
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+
+    }
+}
--- app/src/main/java/com/example/othello/LobbyViewModel.kt
+++ app/src/main/java/com/example/othello/LobbyViewModel.kt
@@ -1,2 +1,29 @@
 package com.example.othello
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import io.garrit.android.multiplayer.Game
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.launch
+
+class LobbyViewModel : ViewModel() {
+
+
+    //BARA BÖRJAN, en mall!
+    sealed class Event {
+        data class StartGame(val opponent: String, val difficulty: String) : Event()
+        data class JoinGame(val game: Game) : Event()
+    }
+
+    private val eventChannel = Channel<Event>(Channel.BUFFERED)
+    val events = eventChannel.receiveAsFlow()
+
+
+
+    fun joinGame(game: Game) {
+        viewModelScope.launch {
+            eventChannel.send(Event.JoinGame(game))
+        }
+    }
+}
