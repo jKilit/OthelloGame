71cfeaf62b8511aa2cf195f75ca342adb701853d
edinnasic <edin.nasic27@gmail.com>
2023-11-23 18:45:52 +0100

    .

--- app/src/main/java/com/example/othello/MainActivity.kt
+++ app/src/main/java/com/example/othello/MainActivity.kt
@@ -1,6 +1,6 @@
 package com.example.othello
 
-import StartScreen
+import LobbyScreen
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -33,6 +33,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -89,7 +90,7 @@ class MainActivity : ComponentActivity() {
                                     composable(route = Screen.Game.route) {
                                         topBarTitle = "Exit Game"
                                         showBackButton = true
-                                        GameScreen(isDarkMode = isDarkMode)
+                                        GameScreen(navController = navController, viewModel=viewModel(), isDarkMode=isDarkMode )
                                     }
                                     composable(route = Screen.GameOver.route) {
                                         topBarTitle = "Game Over"
@@ -106,7 +107,7 @@ class MainActivity : ComponentActivity() {
                                     composable(route = Screen.Lobby.route) {
                                         topBarTitle = "Back to Start screen"
                                         showBackButton = true
-                                        LobbyScreen(navController = navController, isDarkMode )
+                                        LobbyScreen(navController = navController, viewModel = LobbyViewModel(), isDarkMode )
                                     }
                                 }
                             }
