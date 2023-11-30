052b780c6f8b910f06339191a502495981ddfc6b
jonathankilit <Jonathan.kilit@hotmail.com>
2023-11-30 10:46:27 +0100

    Validation changed to max 10

--- app/src/main/java/com/example/othello/StartScreen.kt
+++ app/src/main/java/com/example/othello/StartScreen.kt
@@ -34,7 +34,7 @@ fun Validation(text: String, buttonTitle: String, onValidInput: () -> Unit) {
         onClick = {
             if (text.length < 3) {
                 Toast.makeText(context, "Title more than 3 characters", Toast.LENGTH_LONG).show()
-            } else if (text.length > 120) {
+            } else if (text.length > 10) {
                 Toast.makeText(context, "Text less than 120 characters", Toast.LENGTH_LONG).show()
             } else {
                 onValidInput()
@@ -107,7 +107,6 @@ fun StartScreen(navController: NavController, viewModel: LobbyViewModel, isDarkM
                 )
             }
             Spacer(modifier = Modifier.height(10.dp))
-
             Validation(
                 text = username,
                 buttonTitle = "Join Lobby",
